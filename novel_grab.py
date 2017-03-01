e780c3b2589d9c7267f460106d7fccc765ca4147
winxos <winxos@hotmail.com>
2017-03-01 17:57:20 +0800

    修改部分代码

--- novel_grab.py
+++ novel_grab.py
@@ -16,8 +16,8 @@ import traceback
 from functools import partial
 
 POOLS_SIZE = 20
-try_times = 3
-
+TRY_TIMES = 3
+SINGLE_THREAD_DEBUG = True
 with open('novels_config.json', 'r', encoding='utf-8') as f:
     CONFIG = json.load(f)
 
@@ -26,9 +26,11 @@ def get_chapter(url, loc, RULE_ID):
     try:
         f = urllib.request.urlopen(url)
         c = etree.HTML(f.read().decode(CONFIG["rules"][RULE_ID]["charset"]))
-        # data = c.xpath(loc)[0].xpath("string(.)").strip()
-        data = "".join(c.xpath(loc)).strip()
-        data = data.replace('\xa0', '')
+        # raw elements of div filter none text element
+        raw_txt = ["".join(x.xpath("text()")) for x in c.xpath(loc)]
+        # remove some strange blank.
+        data = "\n".join([t.strip() for t in raw_txt])
+        # data = data.replace('\xa0', '')
         for x in CONFIG["rules"][RULE_ID]["replace"]:
             for src, des in x.items():
                 data = data.replace(src, des)
@@ -44,14 +46,14 @@ def save_txt(name, data, mode='a'):
         f.write(data)
 
 
-def scrape( RULE_ID, chapter_info):
-    global try_times
+def scrape(RULE_ID, chapter_info):
+    global TRY_TIMES
     h, c = chapter_info
     p = get_chapter(h, CONFIG["rules"][RULE_ID]["chapter_content"], RULE_ID)
-    if p == None and try_times > 0:
-        print("[err] retry downloading %d:%s %s" % (try_times, c, h))
-        try_times -= 1
-        _, p = scrape( RULE_ID, chapter_info)
+    if p == None and TRY_TIMES > 0:
+        print("[err] retry downloading %d:%s %s" % (TRY_TIMES, c, h))
+        TRY_TIMES -= 1
+        _, p = scrape(RULE_ID, chapter_info)
     if p == None:
         print("[err] downloaded %s failed. %s" % (c, h))
         p = ""
@@ -77,20 +79,25 @@ def download_novel(url_entry):
     st = clock()
     html = etree.HTML(urllib.request.urlopen(url_entry).read())
     title = html.xpath(CONFIG["rules"][rule_id]["title"])[0].xpath("string(.)")
-    author = html.xpath(CONFIG["rules"][rule_id]["author"])[0].xpath("string(.)")
+    author = html.xpath(CONFIG["rules"][rule_id]["author"])[
+        0].xpath("string(.)")
     file_name = title + " " + author + ".txt"
     href = html.xpath(CONFIG["rules"][rule_id]["chapter_href"])
-    if not str(href[0]).startswith("http"): #not absolute link
+    if not str(href[0]).startswith("http"):  # not absolute link
         href = [server_url + h for h in href]
     capital = html.xpath(CONFIG["rules"][rule_id]["chapter_name"])
     save_txt(file_name, title + "\n" + author + "\n", mode='w')
     chapter_info = zip(href, capital)
     print("[debug] downloading: %s" % file_name)
-    pool = Pool(processes=POOLS_SIZE)
     func = partial(scrape, rule_id)  # wrap multiple arguments
-    results = pool.map(func, chapter_info)
-    pool.close()
-    pool.join()
+    if not SINGLE_THREAD_DEBUG:
+        pool = Pool(processes=POOLS_SIZE)
+        results = pool.map(func, chapter_info)
+        pool.close()
+        pool.join()
+    else:
+        results = map(func, chapter_info)
+
     print('[debug] done. used:%f s' % (clock() - st))
     for c, k in results:
         save_txt(file_name, c + "\n" + k + "\n")
@@ -100,8 +107,7 @@ usage:
 download_novel("your novel chapter lists page link")
 '''
 if __name__ == '__main__':
-    # get_chapter('http://book.zongheng.com/chapter/390021/6494853.html', '//*[@id=\"chapterContent\"]')
     # download_novel('http://book.zongheng.com/showchapter/390021.html')
-    # download_novel('http://www.aoyuge.com/9/9007/index.html')
+    download_novel('http://www.aoyuge.com/9/9007/index.html')
     # download_novel('http://www.quanshu.net/book/38/38215/')
-    download_novel('http://book.zongheng.com/showchapter/403749.html')
+    # download_novel('http://book.zongheng.com/showchapter/403749.html')
